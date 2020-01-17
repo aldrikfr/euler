440a90812826c1493c2ddb27998d8527ebcaa581
Aldrik KLEBER <contact@aldrik.net>
2020-01-17 09:44:41 +0100

    standardization of monoid interface to respect base library guideline

--- src/selfpower/selfpower.ml
+++ src/selfpower/selfpower.ml
@@ -1,15 +1,17 @@
 open Base
 open Distributed_range
 
-let f_map x = Z.pow (Z.of_int x) x
+type t = Z.t
 
-let f_reduce = Z.add
+let self_power x = Z.pow (Z.of_int x) x
 
-let neutral = Z.zero
+let (+) = Z.add
+
+let zero = Z.zero
 
 let of_int x =
   Range.from 1 x
-  |> map_reduce_on_range ~f_reduce ~f_map ~neutral
+  |> map_reduce_on_range ~f_reduce:(+) ~f_map:self_power ~neutral:zero
   |> Z.to_string
 
 let of_string = Fn.compose of_int Int.of_string
