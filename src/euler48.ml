9edacbf8d0a9009825d895034ceaea52f099edba
Aldrik KLEBER <contact@aldrik.net>
2020-01-26 23:13:00 +0100

    ajout gestion d'erreur
    La gestion d'erreur est en place et précise maintenant pourquoi un 
    calcul ne se fait pas.
    
    la librairie stdio est désormais utilisée.

--- src/euler48.ml
+++ src/euler48.ml
@@ -1,8 +1,21 @@
 open Base
+open Stdio
+
+let check_param_available () =
+  if Array.length (Sys.get_argv ())= 2
+  then Result.return 1
+  else Result.fail "One parameter only"
+
+let check_is_int s =
+  try Result.return (Int.of_string s) with
+  | _ -> Result.fail "Parameter is not a numvber"
+
 
 let () =
-  assert ((Array.length Sys.argv) = 2);
-  Sys.argv.(1)
-  |> Selfpower.of_string
-  |> print_string
-  |> print_newline
+  let open Result in
+  check_param_available ()
+  >>= (fun n -> return (Sys.get_argv () |> fun x -> x.(n)) )
+  >>= (Fn.compose return Selfpower.of_string)
+  |> function
+  | Ok s -> Out_channel.(printf "%s\n" s)
+  | Error m -> Out_channel.(eprintf "Erreur:\n%s\n" m )
