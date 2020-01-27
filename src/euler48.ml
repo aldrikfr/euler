13a5d55e7bf9b606ef44d572043478cb6800298c
Aldrik KLEBER <contact@aldrik.net>
2020-01-27 14:08:15 +0100

    remove mix of application and monad pattern
    in order to make easier reading, I only use monad pattern now in the 
    main expression

--- src/euler48.ml
+++ src/euler48.ml
@@ -1,25 +1,29 @@
 open Base
 open Stdio
+open Result
 
 let get_index () =
-  let open Result in
-  match Array.length (Sys.get_argv ()) with
-  | 1 -> fail "A parameter is needed"
+  match Array.length @@ Sys.get_argv @@ () with
   | 2 -> return 1
+  | 1 -> fail "A parameter is needed"
   | _ -> fail "Only one parameter is needed"
 
 let int_of_string s =
-  try (Result.return (Int.of_string s)) with
-  | Failure _ -> Result.fail "Parameter is not a number"
+  try return @@ Int.of_string @@ s with
+  | Failure _ -> fail "Parameter is not a number"
 
-let get_param = Array.get (Sys.get_argv ())
+let get_param x =
+  try return @@ (Array.get @@ Sys.get_argv @@ () ) @@ x with
+  | Invalid_argument _ ->
+    fail ("Internal bug inside get_param, index : " ^ (Int.to_string x))
+
+let get_selfpower x = return @@ Selfpower.of_int @@ x
 
 let () =
-  let open Result in
   get_index ()
-  >>| get_param
+  >>= get_param
   >>= int_of_string
-  >>| Selfpower.of_int
+  >>= get_selfpower
   |> Out_channel.(function
     | Ok s -> printf "%s\n" s
-    | Error m -> eprintf "Error : %s\n" m )
+    | Error m -> eprintf "Error : %s\n" m ; Caml.exit 1)
