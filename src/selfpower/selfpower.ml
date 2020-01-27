a01e9798a4a77b01972b4778ae5c632c4ff2bd36
Aldrik KLEBER <contact@aldrik.net>
2020-01-27 15:45:43 +0100

    refactoring of unit test for handling error message

--- src/selfpower/selfpower.ml
+++ src/selfpower/selfpower.ml
@@ -10,7 +10,7 @@ let (+) = Z.add
 let zero = Z.zero
 
 let of_int x =
-  if x < 1 then Result.fail "Invalid value" else
+  if x < 1 then Result.fail "Non null positive number only" else
   Range.from 1 x
   |> map_reduce_on_range ~f_reduce:(+) ~f_map:self_power ~neutral:zero
   |> Z.to_string
--- test/testselfpower.ml
+++ test/testselfpower.ml
@@ -2,17 +2,25 @@
 (* SPDX-License-Identifier:  GPL-3.0-or-later *)
 open OUnit2
 open Base
+
+let result_printer = Result.( function
+    | Ok s -> s
+    | Error m -> m
+  )
+
 let assert_selfpower expected param =
-  assert_equal ~cmp:(String.equal) ~printer:(Fn.id) expected (Selfpower.of_string param)
+  assert_equal ~cmp:(Result.equal String.equal String.equal ) ~printer:(result_printer) expected (Selfpower.of_string param)
 
 let selfpower_tests =
   [
-    ("with param 1" >:: fun _ctxt -> assert_selfpower "1" "1");
-    ("with param 2" >:: fun _ctxt -> assert_selfpower "5" "2");
+    ("with param 1" >:: fun _ctxt -> assert_selfpower Result.(return "1") "1");
+    ("with param 2" >:: fun _ctxt -> assert_selfpower Result.(return  "5") "2");
+    ("with param -2" >:: fun _ctxt -> assert_selfpower Result.(fail "Non null positive number only") "-2");
+    ("with param -2" >:: fun _ctxt -> assert_selfpower Result.(fail "Non null positive number only") "0");
     ("with big number" >:: fun _ctxt ->
-        assert_selfpower "106876212200059554303215024" "20");
+      assert_selfpower Result.(return "106876212200059554303215024") "20");
     ("equivalence of_int and of_string" >:: fun _ctxt ->
-       assert_equal (Selfpower.of_int 50) (Selfpower.of_string "50")
+      assert_equal (Selfpower.of_int 50) (Selfpower.of_string "50")
     )
   ]
 
