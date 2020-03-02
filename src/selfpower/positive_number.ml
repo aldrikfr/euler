63c58b478570238523410c6e043d982751a6554c
Aldrik KLEBER <contact@aldrik.net>
2020-03-02 22:01:35 +0100

    refactoring to give guarantee of selfpower_on_range result consistency

--- src/selfpower/positive_number.ml
+++ src/selfpower/positive_number.ml
@@ -4,9 +4,10 @@ type t = int
 
 let from_int x =
   Result.(
-    if x < 0 then Error "Integer is negative and can't be transformed to a positive number type"
-    else return x
-  )
+    if x < 0 then
+      Error
+        "Integer is negative and can't be transformed to a positive number type"
+    else return x)
 
 let to_int x = x
 
--- src/selfpower/selfpower.ml
+++ src/selfpower/selfpower.ml
@@ -10,16 +10,16 @@ let ( + ) = Z.add
 
 let zero = Z.zero
 
-let self_power_on_range = function
-  | 0 | 1 -> return "1"
-  | _ as x ->
-      return
-        ( Range.from 1 x
-        |> map_reduce_on_range ~f_reduce:( + ) ~f_map:self_power ~neutral:zero
-        |> Z.to_string )
+let self_power_on_range =
+  Positive_number.map ~f:(function
+    | 0 | 1 -> return "1"
+    | _ as x ->
+        return
+          ( Range.from 1 x
+          |> map_reduce_on_range ~f_reduce:( + ) ~f_map:self_power ~neutral:zero
+          |> Z.to_string ))
 
 (* public part of the code *)
-let of_int x =
-  Positive_number.from_int x >>= Positive_number.map ~f:self_power_on_range
+let of_int x = Positive_number.from_int x >>= self_power_on_range
 
 let of_string = Fn.compose of_int Int.of_string
