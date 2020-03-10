ceff1778047e736dd18fe61330fcce47f69d218a
Aldrik KLEBER <contact@aldrik.net>
2020-03-10 09:58:01 +0100

    code splitting refactoring

--- src/selfpower/selfpower.ml
+++ src/selfpower/selfpower.ml
@@ -10,15 +10,16 @@ let ( + ) = Z.add
 
 let zero = Z.zero
 
-let self_power_on_range =
-  Positive_number.map ~f:(function
-    | 0 | 1 -> return "1"
-    | _ as x ->
-        Range.from 1 x
-        |> map_reduce_on_range ~f_reduce:( + ) ~f_map:self_power ~neutral:zero
-        |> Z.to_string |> return)
+let sum_self_power = function
+  | 0 | 1 -> return "1"
+  | _ as x ->
+      Range.from 1 x
+      |> map_reduce_on_range ~f_reduce:( + ) ~f_map:self_power ~neutral:zero
+      |> Z.to_string |> return
+
+let sum_self_power_of_natural = Positive_number.map ~f:sum_self_power
 
 (* public part of the code *)
-let of_int x = x |> Positive_number.from_int >>= self_power_on_range
+let of_int x = x |> Positive_number.from_int >>= sum_self_power_of_natural
 
 let of_string = Fn.compose of_int Int.of_string
