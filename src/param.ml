0591a79455fb31f9537e4f09a534ea66e74ed81a
Aldrik KLEBER <contact@aldrik.net>
2020-03-22 22:09:55 +0100

    refactoring for removing excessive use of @@ operator

--- src/param.ml
+++ src/param.ml
@@ -1,18 +1,20 @@
 open Base
 open Result
 
+let argv = Sys.get_argv ()
+
 let get_idx_if_available () =
-  match Array.length @@ Sys.get_argv @@ () with
+  match Array.length argv with
   | 2 -> return 1
   | 1 -> fail "A parameter is needed"
   | _ -> fail "Only one parameter is needed"
 
 let int_of_string s =
-  try return @@ Int.of_string @@ s
+  try s |> Int.of_string |> return
   with Failure _ -> fail "Parameter is not a number"
 
 let get x =
-  try return @@ (Array.get @@ Sys.get_argv @@ ()) @@ x
+  try return (Array.get argv x)
   with Invalid_argument _ ->
     fail ("Internal bug inside get_param, index : " ^ Int.to_string x)
 
