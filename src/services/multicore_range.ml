0ac29a1b465772253c8552abb34754a3d7133c71
Aldrik KLEBER <contact@aldrik.net>
2020-04-26 10:17:03 +0200

    migration of code to be compatible with range 0.8.3

--- src/services/multicore_range.ml
+++ src/services/multicore_range.ml
@@ -1,14 +1,16 @@
 open Functory.Cores
 open Base
 
-let cores_available = 
+let cores_available =
   match (Sys.getenv "CORES_AVAILABLE" ) with
     | None -> 6
-    | Some s -> Int.of_string s
+    | Some s -> Int.of_string s |> abs
 
-let threshold = 1_000
+let threshold = 1_000 |> Range.Number.gtz_from_int_exn
 
-let batch_job_number = cores_available * 2
+let batch_job_number =
+  cores_available * 2
+  |> Range.Number.gtz_from_int_exn
 
 let () = set_number_of_cores cores_available
 
@@ -16,5 +18,5 @@ let map_reduce_on_range ~f_map ~f_reduce ~neutral r =
   r
   |> Range.split threshold batch_job_number
   |> map_fold_ac
-       ~f:(Range.fold (fun acc x -> f_reduce acc (f_map x)) neutral)
+       ~f:(Range.fold ~f:(fun acc x -> f_reduce acc (f_map x)) neutral)
        ~fold:f_reduce neutral
