0a1ec0b013092eafbe287bf5f0e18de236ae452a
Aldrik KLEBER <contact@aldrik.net>
2020-04-27 00:08:04 +0200

    better error management in multicore configuration

--- src/services/multicore_range.ml
+++ src/services/multicore_range.ml
@@ -1,22 +1,31 @@
 open Functory.Cores
 open Base
 
-let cores_available =
-  match (Sys.getenv "CORES_AVAILABLE" ) with
-    | None -> 6
-    | Some s -> Int.of_string s |> abs
+module Num = Range.Number
+let cores_available_default_value = 6
 
-let threshold = 1_000 |> Range.Number.gtz_from_int_exn
+let get_cores_setting () =
+  let open Option in
+  (Sys.getenv "CORES_AVAILABLE" )
+  >>= (fun s -> try Int.of_string s |> some with _ -> None)
+  >>= (fun x -> some_if (x > 0) x)
+  |> value ~default:cores_available_default_value
+
+let cores_available = get_cores_setting ()
+
+let threshold = 1_000 |> Num.gtz_from_int_exn
 
 let batch_job_number =
   cores_available * 2
-  |> Range.Number.gtz_from_int_exn
+  |> Num.gtz_from_int_exn
 
 let () = set_number_of_cores cores_available
 
 let map_reduce_on_range ~f_map ~f_reduce ~neutral r =
+  let map_reduce_of_range_value acc x =
+    f_reduce acc (f_map x) in
   r
   |> Range.split threshold batch_job_number
   |> map_fold_ac
-       ~f:(Range.fold ~f:(fun acc x -> f_reduce acc (f_map x)) neutral)
+       ~f:(Range.fold ~f:map_reduce_of_range_value neutral)
        ~fold:f_reduce neutral
