d8df017dc369a28a7a9947426ca0224ea5826553
Aldrik KLEBER <contact@aldrik.net>
2020-04-28 23:11:20 +0200

    limit max number to avoid exception caused by gmp limitation and proper error handling for too big number

--- src/selfpower/positive_number.ml
+++ src/selfpower/positive_number.ml
@@ -3,7 +3,11 @@ open Base
 type t = int
 
 let from_int x =
-  Result.(if x < 0 then Error "Positive number only" else return x)
+  let open Result in
+  if x < 0 then Error "Positive number only"
+  else if x > 100_000_000 then
+    Error "Too big number for actual multi-precision library"
+  else return x
 
 let to_int x = x
 
