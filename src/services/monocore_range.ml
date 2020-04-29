5cb61eeb3102e2a80fc93a9f1f521ca831aa7b72
Aldrik KLEBER <contact@aldrik.net>
2020-04-29 15:15:19 +0200

    monocore compatible with range 0.8.3

--- src/services/monocore_range.ml
+++ src/services/monocore_range.ml
@@ -1,2 +1,2 @@
-let map_reduce_on_range ~f_map ~f_reduce ~neutral =
-  Range.fold (fun acc x -> f_reduce acc (f_map x)) neutral
+let map_reduce_on_range ~f_map ~f_reduce ~neutral r =
+  Range.fold ~f:(fun acc x -> f_reduce acc (f_map x)) neutral r
