a46f2650182a808e3ff2a4508e5010bb62832f59
Aldrik KLEBER <contact@aldrik.net>
2020-04-30 18:07:20 +0200

    add of let-binding usage

--- src/param.ml
+++ src/param.ml
@@ -2,6 +2,11 @@ open Base
 open Result
 open Stdio
 
+ let ( let* ) r f =
+   match r with
+   | Error s -> Error s
+   | Ok x -> f x
+
 let argv = Sys.get_argv ()
 
 let get_idx_if_available () =
@@ -12,16 +17,18 @@ let get_idx_if_available () =
 
 let int_of_string s =
   try return @@ Int.of_string @@ s
-  with Failure _ -> fail ("Parameter is not a number : " ^ s)
+  with Failure _ -> fail ("Input can't be converted to a number")
 
 let get x =
-  ( try return argv.(x)
+  let* argument = try return argv.(x)
     with Invalid_argument _ ->
-      fail ("Internal bug inside get_param, index : " ^ Int.to_string x) )
-  >>= fun s ->
-  if String.equal s "--" then
+      fail ("Internal bug inside get_param, index : " ^ Int.to_string x) in
+  if String.equal argument "--" then
     In_channel.(input_line stdin)
     |> Option.value_map ~f:return ~default:(fail "problem reading stdin")
-  else return s
+  else return argument
 
-let get_number () = get_idx_if_available () >>= get >>= int_of_string
+let get_number () =
+  let* idx = get_idx_if_available () in
+  let* s = get idx in
+  int_of_string s
