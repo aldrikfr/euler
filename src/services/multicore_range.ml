1d85695198c3818fbeb1f3e26136754f6ab92920
Aldrik KLEBER <contact@aldrik.net>
2020-07-15 10:23:01 +0200

    monocore is used dynamically when 1 core is requested

--- src/services/multicore_range.ml
+++ src/services/multicore_range.ml
@@ -2,19 +2,7 @@ open Functory.Cores
 open Base
 module Num = Range.Number
 
-let cores_available_default_value = 6
-
-let int_from_string s = try Int.of_string s |> Option.some with _ -> None
-
-let valid_core_value n = Option.some_if Range.(from 1 256 |> contain n) n
-
-let get_cores_setting () =
-  let open Option in
-  Sys.getenv "CORES_AVAILABLE"
-  >>= int_from_string >>= valid_core_value
-  |> value ~default:cores_available_default_value
-
-let cores_available = get_cores_setting ()
+let cores_available = Setting.get_cores_to_use ()
 
 let threshold = 1_000 |> Num.gtz_from_int_exn
 
--- src/services/range_processing.ml
+++ src/services/range_processing.ml
@@ -1,1 +1,2 @@
-let map_reduce_on_range = Multicore_range.map_reduce_on_range
+let map_reduce_on_range = 
+  if Setting.get_cores_to_use () < 2 then Monocore_range.map_reduce_on_range else Multicore_range.map_reduce_on_range
