8290cac0ec255d505388ac686bd72f25f1136762
Aldrik KLEBER <contact@aldrik.net>
2020-07-15 15:14:09 +0200

    refactoring of setting service opening widely option module

--- src/services/setting.ml
+++ src/services/setting.ml
@@ -1,13 +1,12 @@
 open Base
+open Option
+let int_from_string s = try some @@ Int.of_string @@ s with _ -> None
 
-let int_from_string s = try Int.of_string s |> Option.some with _ -> None
-
-let valid_core_value n = Option.some_if Range.(from 1 256 |> contain n) n
+let valid_core_value n = some_if Range.(from 1 256 |> contain n) n
 
 let cores_available_default_value = 6
 
 let get_cores_to_use () =
-  let open Option in
   Sys.getenv "CORES_AVAILABLE"
   >>= int_from_string >>= valid_core_value
   |> value ~default:cores_available_default_value
