cfb0cb41fb923910b4bbc588bc51806e8b85c615
Aldrik KLEBER <contact@aldrik.net>
2020-07-15 15:14:46 +0200

    local map reduce in multicore now reuse monocore implementation

--- src/services/multicore_range.ml
+++ src/services/multicore_range.ml
@@ -1,6 +1,7 @@
 open Functory.Cores
 open Base
 module Num = Range.Number
+module Mono = Monocore_range
 
 let cores_available = Setting.get_cores_to_use ()
 
@@ -11,9 +12,8 @@ let batch_job_number = cores_available * 2 |> Num.gtz_from_int_exn
 let () = set_number_of_cores cores_available
 
 let map_reduce_on_range ~f_map ~f_reduce ~neutral r =
-  let map_reduce_inside_list acc x = f_reduce acc (f_map x) in
   r
   |> Range.split threshold batch_job_number
   |> map_fold_ac
-       ~f:(Range.fold ~f:map_reduce_inside_list neutral)
+       ~f:(Mono.map_reduce_on_range ~f_map ~f_reduce ~neutral)
        ~fold:f_reduce neutral
