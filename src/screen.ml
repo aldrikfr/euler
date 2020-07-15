dd37ff7f6c87f3fe98cf7ed40c765c941ee8c6bb
Aldrik KLEBER <contact@aldrik.net>
2020-07-15 10:20:01 +0200

    refactoring for avoiding using directly option type implementation

--- src/screen.ml
+++ src/screen.ml
@@ -1,8 +1,12 @@
 open Base
 open Stdio
+module R = Result
 
-let handle_result = function
-  | Ok s -> Out_channel.printf "%s\n" s
-  | Error m ->
-      Out_channel.eprintf "Error : %s\n" m;
-      Caml.exit 1
+let string_to_stdout = Out_channel.printf "%s\n"
+
+let string_to_stderr m =
+  Out_channel.eprintf "Error : %s\n" m;
+  Caml.exit 1
+
+let handle_result r =
+  R.map ~f:string_to_stdout r |> R.map_error ~f:string_to_stderr |> ignore
