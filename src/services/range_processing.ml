52c9efe4a970a6ae9a94178528cc7ea7f22ae162
Aldrik KLEBER <contact@aldrik.net>
2020-08-05 22:53:18 +0200

    use of equality of Int base module

--- src/services/range_processing.ml
+++ src/services/range_processing.ml
@@ -1,3 +1,5 @@
+open Base
+
 let map_reduce_on_range =
-  if Setting.get_cores_to_use () < 2 then Monocore_range.map_reduce_on_range
+  if Int.(Setting.get_cores_to_use () = 1) then Monocore_range.map_reduce_on_range
   else Multicore_range.map_reduce_on_range
