37fcf2d941bfd96d043589ecc8404f04c3fe0ab8
Aldrik KLEBER <contact@aldrik.net>
2020-11-03 11:17:37 +0100

    strengthening of environment configuration checking in order to fail more quickly

--- src/services/setting.ml
+++ src/services/setting.ml
@@ -7,7 +7,12 @@ let valid_core_value n = some_if Range.(from 1 256 |> contain n) n
 
 let cores_available_default_value = 6
 
+let is_inside_int_length s =
+  if String.length s <= Int.(max_value |> to_string |> String.length) then
+    Some s
+  else None
+
 let get_cores_to_use () =
   Sys.getenv "CORES_AVAILABLE"
-  >>= int_from_string >>= valid_core_value
+  >>= is_inside_int_length >>= int_from_string >>= valid_core_value
   |> value ~default:cores_available_default_value
