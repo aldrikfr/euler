cf51ee9b5f0c098a9c2b734c98a2dd8eca5b1d98
Aldrik KLEBER <contact@aldrik.net>
2020-11-03 12:53:17 +0100

    suppression of unuseful ocaml begin/end instructions

--- src/screen.ml
+++ src/screen.ml
@@ -3,16 +3,12 @@ open Stdio
 module R = Result
 
 let string_to_stdout m =
-begin
   Out_channel.printf "%s\n" m;
   Caml.exit 0
-end
 
 let string_to_stderr m =
-begin
   Out_channel.eprintf "Error : %s\n" m;
   Caml.exit 1
-end
 
 let handle_result r =
   R.map ~f:string_to_stdout r |> R.map_error ~f:string_to_stderr |> ignore
