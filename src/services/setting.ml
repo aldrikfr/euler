72c5e6e538196e1a132fe554c48c1b1b10af16b3
Aldrik KLEBER <contact@aldrik.net>
2020-12-19 22:25:27 +0100

    moving constant values calculation outside of runtime calculation

--- src/services/setting.ml
+++ src/services/setting.ml
@@ -6,13 +6,15 @@ let max_parallel_cpu_supported = 256
 
 let cores_available_default_value = 6
 
+let max_int_string_length = Int.(to_string max_value) |> String.length
+
 let int_from_string s = try some @@ Int.of_string @@ s with _ -> None
 
 let valid_core_value n =
   some_if Range.(from 1 max_parallel_cpu_supported |> contain n) n
 
 let is_inside_int_length s =
-  some_if (String.length s <= Int.(max_value |> to_string |> String.length)) s
+  some_if (String.length s <= max_int_string_length) s
 
 let get_cores_to_use () =
   Sys.getenv "CORES_AVAILABLE"
