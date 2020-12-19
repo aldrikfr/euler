35a576f6aa05e95ebc6b8b1ec91ed34c775b768e
Aldrik KLEBER <contact@aldrik.net>
2020-12-19 22:45:11 +0100

    for predicate tests usage of filter option instead of some_if

--- src/services/setting.ml
+++ src/services/setting.ml
@@ -10,13 +10,13 @@ let max_int_string_length = Int.(to_string max_value) |> String.length
 
 let int_from_string s = try some @@ Int.of_string @@ s with _ -> None
 
-let valid_core_value n =
-  some_if Range.(from 1 max_parallel_cpu_supported |> contain n) n
+let is_core_value_valid n = Range.(from 1 max_parallel_cpu_supported |> contain n)
 
-let is_inside_int_length s =
-  some_if (String.length s <= max_int_string_length) s
+let is_inside_int_length s = String.length s <= max_int_string_length
 
 let get_cores_to_use () =
   Sys.getenv "CORES_AVAILABLE"
-  >>= is_inside_int_length >>= int_from_string >>= valid_core_value
+  |> filter ~f:is_inside_int_length
+  >>= int_from_string
+  |> filter ~f:is_core_value_valid
   |> value ~default:cores_available_default_value
