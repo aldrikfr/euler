cf1f48efd60498a3604db55d2745401e95e16fd5
Aldrik <contact@aldrik.net>
2022-01-27 21:14:43 +0100

    small rename adjustment to be more consistent with optimization of max string length optimization

--- src/services/setting.ml
+++ src/services/setting.ml
@@ -6,7 +6,7 @@ let max_parallel_cpu_supported = 256
 
 let cores_available_default_value = 6
 
-let max_int_string_length =
+let max_string_length =
   Int.to_string max_parallel_cpu_supported |> String.length
 
 
@@ -14,7 +14,7 @@ let int_from_string s () = Int.of_string s
 
 let is_under bound n = Range.(from 1 bound |> contain n)
 
-let is_inside_int_length s = String.length s <= max_int_string_length
+let is_inside_int_length s = String.length s <= max_string_length
 
 let get_cores_to_use () =
   Sys.getenv "CORES_AVAILABLE"
